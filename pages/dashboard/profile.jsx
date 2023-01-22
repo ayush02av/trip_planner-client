79a53fc80af64d43d27c8f611ee0fd3ced80fa8d
unknown <ayush02av@gmail.com>
2023-01-22 15:01:29 +0530

    Profile error handling

--- pages/dashboard/profile.jsx
+++ pages/dashboard/profile.jsx
@@ -16,16 +16,17 @@ export default function Profile() {
     const userProfile = useSelector(state => state.userProfile.value)
 
     return (
-        <div>
-            <div className="text-3xl font-bold mb-5">Profile</div>
-            {profileDisplayItems.map((profileDisplayItem, index) => (
-                <div key={index}>
-                    <span className="font-semibold">{profileDisplayItem.title}</span>
-                    :&nbsp;
-                    <span>{userProfile[profileDisplayItem.key]}</span>
-                </div>
-            ))}
-        </div >
+        (!userProfile) ? (<>loading</>) :
+            <div>
+                <div className="text-3xl font-bold mb-5">Profile</div>
+                {profileDisplayItems.map((profileDisplayItem, index) => (
+                    <div key={index}>
+                        <span className="font-semibold">{profileDisplayItem.title}</span>
+                        :&nbsp;
+                        <span>{userProfile[profileDisplayItem.key]}</span>
+                    </div>
+                ))}
+            </div >
     )
 }
 
